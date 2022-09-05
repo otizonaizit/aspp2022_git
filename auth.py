c2e4017249ff9b4b87d5a74f2f20baf799ab1a54
Tiziano Zito <opossumnano@gmail.com>
2022-09-05 10:37:52 +0200

    added the db dump function

--- auth.py
+++ auth.py
@@ -1,8 +1,17 @@
+import json
+
 def get_credentials():
     username = input('Enter your username: ')
     password = input(f'Enter your password {username}: ')
 
     return username, password
 
+def write_passwdb(passwd):
+    with open('passwd.json', 'w') as pwdb_file:
+        json.dump(pwdb, pwdb_file)
+
+
 if __name__ == '__main__':
-    get_credentials()
+    username, password = get_credentials()
+    pwdb = {username: password}
+    write_passwdb(pwdb)
