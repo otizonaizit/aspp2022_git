7105d6993cf340126a077b0aa1a5e0beb25d4492
Tiziano Zito <opossumnano@gmail.com>
2022-09-05 10:45:45 +0200

    first complete version

--- auth.py
+++ auth.py
@@ -6,12 +6,33 @@ def get_credentials():
 
     return username, password
 
-def write_passwdb(passwd):
+def read_passwdb():
+    with open('passwd.json', 'r') as pwdb_file:
+        pwdb = json.load(pwdb_file)
+    return pwdb
+
+def write_passwdb(pwdb):
     with open('passwd.json', 'w') as pwdb_file:
         json.dump(pwdb, pwdb_file)
 
+def authenticate(username, password, pwdb):
+    if username in pwdb:
+        if password == pwdb[username]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def main():
+    username, password = get_credentials()
+    pwdb = read_passwdb()
+    status = authenticate(username, password, pwdb)
+    if status:
+        print('Success!')
+    else:
+        print('Wrong username or password!')
+    write_passwdb(pwdb)
 
 if __name__ == '__main__':
-    username, password = get_credentials()
-    pwdb = {username: password}
-    write_passwdb(pwdb)
+    main()
